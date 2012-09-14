5e766685bb29d8f703634282bc1da57b65a527a3
Gasol Wu <gasol.wu@gmail.com>
2012-09-14 23:40:54 +0800

    Inject TextView and setText

--- android/src/tw/gasol/android/MainActivity.java
+++ android/src/tw/gasol/android/MainActivity.java
@@ -1,12 +1,19 @@
 package tw.gasol.android;
 
 import roboguice.activity.RoboActivity;
+import roboguice.inject.InjectView;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends RoboActivity {
+
+	@InjectView(R.id.text)
+	private TextView mTextView;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		mTextView.setText(getString(R.string.hello_roboguice));
 	}
 }
